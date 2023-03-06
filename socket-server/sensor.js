bac30b63a7b89af60d2481acebf5204370a31f48
john <jfinberg@haverford.edu>
2023-03-06 11:56:31 -0500

    temp

--- socket-server/sensor.js
+++ socket-server/sensor.js
@@ -1,27 +1,33 @@
-const gpio = require('onoff').Gpio;
+const gpio = require('rpi-gpio');
 
-const tempSensor = new gpio(17, 'in', 'both');
-const lightSensor = new gpio(27, 'in', 'both');
+// Set up the GPIO pin connected to the temperature sensor
+const sensorPin = 17; // Change this to the GPIO pin number that your sensor is connected to
+gpio.setup(sensorPin, gpio.DIR_IN, gpio.EDGE_BOTH);
 
-tempSensor.watch(function (err, value) { //Watch for hardware interrupts on pushButton GPIO, specify callback function
-  if (err) { //if an error
-    console.error('There was an error', err); //output error message to console
-  return;
+// Define a function to handle changes in the sensor input
+function onSensorChange(err, value) {
+  if (err) {
+    throw err;
   }
-  console.log(`The tempature sensor value is ${value}`);
-});
+  console.log('Temperature sensor value changed to:', value);
+  // TODO: Perform temperature conversion/calculations on the sensor value here
+}
 
-lightSensor.watch(function (err, value) { //Watch for hardware interrupts on pushButton GPIO, specify callback function
-  if (err) { //if an error
-    console.error('There was an error', err); //output error message to console
-  return;
-  }
-  console.log(`The tempature sensor value is ${value}`);
-});
-  
-function unexportOnClose() { //function to run when exiting program
-  tempSensor.unexport(); // Unexport Button GPIO to free resources
-  lightSensor.unexport(); // Unexport Button GPIO to free resources
-};
-  
-process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+c
+// Add a listener for changes in the sensor input
+gpio.on('change', onSensorChange);
+
+// Optional: Set up a periodic timer to read the sensor input at regular intervals
+const readIntervalMs = 1000; // Change this to set the desired interval for reading the sensor
+setInterval(() => {
+  gpio.read(sensorPin, (err, value) => {
+    if (err) {
+      throw err;
+    }
+    console.log('Current temperature sensor value:', value);
+    // TODO: Perform temperature conversion/calculations on the sensor value here
+  });
+}, readIntervalMs);
+
+
+// const tempSensor = new gpio(17, 'in', 'both');
+// const lightSensor = new gpio(27, 'in', 'both');
