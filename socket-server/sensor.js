0d2a72d47f7e5529bfcc8c39e58a7d37978dd1a2
john <jfinberg@haverford.edu>
2023-03-13 17:11:48 -0400

    port

--- socket-server/sensor.js
+++ socket-server/sensor.js
@@ -1,14 +1,6 @@
-const SerialPort = require('serialport').SerialPort;
-const Readline = require('@serialport/parser-readline');
-const port = new SerialPort('/dev/ttyACM0', {
-  baudRate: 9600
-});
-
-const parser = port.pipe(new Readline({ delimiter: '\n' }));
-// Read the port data
-port.on("open", () => {
-  console.log('serial port open');
-});
-parser.on('data', data =>{
-  console.log('got word from arduino:', data);
-});
+const pathName = '/dev/ttyACM0';
+const { SerialPort, ReadlineParser } = require('serialport')
+const port = new SerialPort({ 'path': pathName,  'baudRate': 9600 })
+const parser = new ReadlineParser()
+port.pipe(parser)
+parser.on('data', console.log)
